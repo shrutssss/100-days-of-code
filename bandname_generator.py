ad5ed77df7d248c7debdf8103362e7130e337333
shrutssss <shrutiv.jahagirdar@gmail.com>
2025-07-22 17:58:15 +0530

    first commit

---
+++ bandname_generator.py
@@ -0,0 +1,5 @@
+#Basic band name generator using city name and pet names.
+print("Welcome to Band Generator!")
+city_name=input("Which city did you grow up in?\n")
+pet_name=input("What was your childhood pet's name?\n")
+print(f"Your band name should be: {city_name}{pet_name}")
