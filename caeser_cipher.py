1ab10d8afd54c5eaaebe89333dcad42b9196a1c3
shrutssss <shrutiv.jahagirdar@gmail.com>
2025-07-27 12:34:23 +0530

    final commit

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -1,17 +1,38 @@
-#Declaration of required variables
+print(''' ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     'Y8 a8P_____88 I8[    "" ""     'Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  '"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ '"Ybbd8"' '"8bbdP"Y8  '"Ybbd8"' '"YbbdP"' '"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ '"Ybbd8"' 88 88'YbbdP"'  88       88  '"Ybbd8"' 88          
+              88                                             
+              88    ''')
+
+
+#Declaration of required variables and functions
 alphabets=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-choice=input("Type 'encode' to encrpt and decode to decrypt:\n").lower()
+
+def caeser(original_text,shift_amount,direction):
+    output_text=""
+    if direction=="decode":
+            shift_amount*=-1 
+    for letter in original_text:
+        if letter in alphabets:
+            shifted_position=alphabets.index(letter)+shift_amount
+            shifted_position%=len(alphabets)
+            output_text+=alphabets[shifted_position]
+        else:
+            output_text+=letter
+    print(f"Here is the result: {output_text}")
+
+choice=input("Type 'encode' to encrypt and 'decode' to decrypt:\n").lower()
 text=input(f"Enter text to {choice}:\n").lower()
 shift=int(input("Enter shift number:\n"))
 
-#Encrypt
-def Encrypt(original_text,shift_amount):
-    encrypted_text=""
-    for letter in original_text:
-        shifted_position=alphabets.index(letter)+shift_amount
-        encrpted_text+=alphabets[shifted_position]
-    print(encrypted_text)
-                
-if choice=="encode":
-    Encrypt(text,shift)
-            
+caeser(text,shift,choice)
